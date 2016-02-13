a5f7e6e05008df6e2b34af45593f81d4e161a83f
Alain DEMENET <ademenet@e2r8p13.42.fr>
2016-02-13 17:16:34 +0100

    finish money List

--- poney/PoneyListViewController.swift
+++ poney/PoneyListViewController.swift
@@ -8,28 +8,45 @@
 
 import UIKit
 
-class PoneyListViewController: UIViewController {
+struct Poney {
+  var name: String
+  var description: String
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  init(name: String, description: String) {
+    self.name = name
+    self.description = description
+  }
+}
 
-        // Do any additional setup after loading the view.
-    }
+class PoneyListViewController: UITableViewController {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+  var poneys = [Poney]()
 
-    /*
-    // MARK: - Navigation
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    // Do any additional setup after loading the view.
+    let poney1 = Poney(name: "bob", description: "joly")
+    let poney2 = Poney(name: "alain", description: "moche")
+    poneys += [poney1, poney2]
+  }
+
+  // Table View Protocol
+
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return poneys.count
+  }
+
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cellIdentifier = "PoneyCellIdentifier"
+    let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as! PoneyCell
+    cell.nameLabel.text = poneys[indexPath.row].name
+    cell.descriptionLabel.text = poneys[indexPath.row].description
+    return cell
+  }
 
 }
