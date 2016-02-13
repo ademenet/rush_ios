cb6aa736bdb7ab718396b606166db8583469efbe
Alain DEMENET <ademenet@e2r8p13.42.fr>
2016-02-13 11:18:33 +0100

    Initial Commit

---
+++ poney/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  poney
+//
+//  Created by Alain DEMENET on 2/13/16.
+//  Copyright © 2016 Alain DEMENET. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
